9593446f5bd5f68c6293cac475b3fed02e368ee0
Rávilon <94138486+ravilon@users.noreply.github.com>
2022-04-28 11:02:50 -0300

    Add files via upload

---
+++ TriagemHospitalar/TriagemHospitalar/Lista.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriagemHospitalar
+{
+    internal class Lista
+    {
+        public Lista()
+        {
+            this.IniciarLista();
+        }
+
+        // Cria uma lista de pacientes
+
+        private List<Pacient> pacients; 
+
+        public List<Pacient> Pacientes       
+        {
+            get { return pacients; }
+        }
+
+        private void IniciarLista() 
+        { 
+            this.pacients = new List<Pacient>(); 
+        } 
+
+        // Método
+        // Listar pacientes
+
+        public void ListPacients()
+        {
+            for (int i = 0; i < this.pacients.Count; i++)
+            {
+                
+                Pacient p = this.Pacientes[i];
+                p.ShowStats();
+            }
+        }
+    }
+}
